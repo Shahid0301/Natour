4b806684708bb0eb1340332fe6b3b441b719fd04
Shahid khan <professionalshahid0@gmail.com>
2024-02-12 13:33:26 +0530

    Structured files and added some middlewares

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express');
 
-
 const app = express();
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 // middleware....
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  
+}
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/public`))
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
 // app.post('/api/v1/tours', createTour);
 // app.patch('/api/v1/tours/id', updateTour);
 // app.delete('/api/v1/tour', deleteTour);\
-
-
+app.use((req, res, next) => {
+  console.log("hello from the middleware");
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 app.use('/api/v1/tours', tourRouter);
-app.use('api/v1/users', userRouter);
-const port = 3000;
-app.listen(port, () => {
-  console.log('app running on port ');
-});
+app.use('/api/v1/users', userRouter);
+module.exports = app;
