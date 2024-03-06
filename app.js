73702a3aaa4ae35134b560f2967e4c6b482f65ad
Shahid khan <professionalshahid0@gmail.com>
2024-03-06 15:36:47 +0530

    Implemented Express Error handling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const AppError = require('./utils/appError');
+const globalErrorHandler=require('./controllers/errorController')
 const app = express();
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
@@ -16,15 +17,30 @@ app.use(express.static(`${__dirname}/public`))
 // app.post('/api/v1/tours', createTour);
 // app.patch('/api/v1/tours/id', updateTour);
 // app.delete('/api/v1/tour', deleteTour);\
+// app.use((req, res, next) => {
+//   console.log("hello from the middleware");
+//   next();
+// });
 app.use((req, res, next) => {
-  console.log("hello from the middleware");
-  next();
-});
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
+  req.requestTime = new Date().toISOString(); 
   next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//not specified route then
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'failed',
+  //   message: `Cant find ${req.originalUrl} on this server!`
+  // })
+  // const err = new Error(`Cant find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  //if any argument passed to next then it knows that it will be an error
+  next(new AppError(`Cant find ${req.originalUrl} on this server!`,404));
+});
+//if we specify four parameter then express know that it is for error handingling
+app.use(globalErrorHandler);
 module.exports = app;
--- server.js
+++ server.js
@@ -5,9 +5,15 @@ dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
-);
-const app = require('./app');
-// console.log( process.env);
+  );
+  const app = require('./app');
+  // console.log( process.env);
+  process.on('uncaughtException', err => {
+    console.log('Uncaught Excepitons! Shutting down');
+    console.log(err.name, err.message);
+    process.exit(1);
+  
+  });
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -19,6 +25,15 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('app running on port ');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection Shutting down');
+  server.close(() => {
+    process.exit(1);
+  });
+
 });
